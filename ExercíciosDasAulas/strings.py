7918c4723929e76efd81f0730e4de0cb43c3d645
Caio Neves <ncaio037@gmail.com>
2022-06-28 10:08:47 -0300

    .

---
+++ ExercíciosDasAulas/strings.py
@@ -0,0 +1,9 @@
+nome1 = 'caio'
+nome2 = 'neves'
+num1 = 5
+NomeCompleto = nome1 + " " + nome2
+print('Olá %s %i, tudo bem?' %(NomeCompleto,num1))
+print( "Olá\n como você está?\n oiiii\n")
+
+#o \n serve para pular uma linha entre strings
+# % serve para mudar o luvar da variável de acordo com o tipo de variável.
---
+++ ExercíciosDasAulas/variáveis.py
@@ -0,0 +1,7 @@
+from cgitb import text
+
+
+num = 10
+num2 = 10.5
+texto = ('alguma coisa')
+print(type(texto))
