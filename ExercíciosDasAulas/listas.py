af75c2b661527c9fde06880475ded2d0227646f2
Caio Neves <ncaio037@gmail.com>
2022-06-28 10:47:13 -0300

    Trabalhando e modificando listas.

---
+++ ExercíciosDasAulas/listas.py
@@ -0,0 +1,33 @@
+# a númeração em python sempre começará pelo 0.
+
+carros = ['Ferrari', 'Lamborghini', 'Camaro']
+
+print("O meu carro favorito é o " + carros[-1].title())
+
+# o -1 representa o último elemento de uma lista.
+
+carros[1] = 'Mustang'
+carros.append('BMW')
+carros.insert(0, 'Jeep')
+carros.extend('Fusca')
+del carros[0]
+carros.pop(2)
+carros.remove('Mustang')
+
+print(carros)
+
+
+# Adicionando elementos ao final da lista com o .append
+
+#inserindo elementos na lista com posição definida utilizando o .insert
+
+# Alterando elementos da lista.
+
+# O .extend separa os caracteres e insere na lista de forma separada.
+
+# O del deleta um elemento da lista de acordo com a posição indicada.
+
+# O .pop deleta o último elemento da lista.
+
+# O .remove procura o elemento e o elimina.
+
---
+++ ExercíciosDasAulas/numeros.py
@@ -0,0 +1,10 @@
+palavra1 = 'Olá'
+palavra2 = ', Meu nome é '
+palavra3 = 'Caio'
+palavra4 = ' e tenho '
+num1 = 19
+palavra5 = ' anos'
+
+print(palavra1 + palavra2 + palavra3 + palavra4 + str(num1) + palavra5)
+
+#entendendo a concatenação e a declaração dos tipos de variáveis. 
