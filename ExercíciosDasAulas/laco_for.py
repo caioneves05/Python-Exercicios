87eccb9230c034deb4c9b3072d763922f2cc17d8
Caio Neves <ncaio037@gmail.com>
2022-06-29 10:59:44 -0300

    .

---
+++ ExercíciosDasAulas/laco_for.py
@@ -0,0 +1,6 @@
+carros = ['Ferrari', 'Mustang', 'BMW', 'Lamborghini', 'Civic', 'Corolla', 'Honda Fit']
+
+for carro in carros: 
+    print('Eu quero um carro do modelo ' + carro)
+
+# Com o for carro in carros: ele cria um laço de repetição dentro da lista carros, sendo o CARRO uma variável que só exite dentro do for.
---
+++ ExercíciosDasAulas/laco_for2.py
@@ -0,0 +1,10 @@
+numeros = []
+for n in range(1,101,):
+    numeros.append(n)
+print(numeros)
+
+# O list transforma a lista em variáveis.
+
+# O range gera uma sequência numérica em um determinado intervalo.
+
+# O 4 acima demonstra o intervalo entre os números, exemplo '4, 8, 12, 16, 20'
