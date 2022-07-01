8d871d491823f6d4abe09fdad6f841f9739d5b88
Caio Neves <ncaio037@gmail.com>
2022-07-01 00:35:53 -0300

    .

---
+++ ExercíciosDasAulas/copiando_listas.py
@@ -0,0 +1,8 @@
+alimentos = ['tomate', 'batata', 'pao', 'pizza', 'ovo', 'arroz']
+alimentos2 = alimentos[:]
+alimentos.append('Feijão')
+alimentos2.sort()
+print(alimentos)
+print(alimentos2)
+
+# [:] seleciona todos os elementos da lista.
---
+++ ExercíciosDasAulas/fatiando_listas.py
@@ -0,0 +1,10 @@
+alimentos = ['tomate', 'batata', 'pao', 'pizza', 'ovo', 'arroz']
+
+for alimento in alimentos[3:5]:
+    print(alimento)
+
+# O [2:3] serve para percorrer pela a lista entre os elementos.
+
+# [2:3:4] sendo o 4 os intervalos selecionados entre um número e outro.
+
+# [-3:] nesse caso ele pegou os 3 últimos elementos da lista utilizando o -3
